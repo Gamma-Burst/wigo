56d6ad8cb6136a26fc9b0c2dc9367415c394546e
Gamma-Burst <christophe.v.mercier@gmail.com>
2026-04-18 19:20:42 +0200

    chore: remove test files

--- test-geo.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: '.env' });
-
-async function testGeo() {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=Ardennes&key=${process.env.GOOGLE_API_KEY}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(JSON.stringify(data.results[0]?.geometry?.location));
-}
-testGeo();
